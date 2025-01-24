50719d9c210085405c4f223410c62e0b55869ad7
“imm35” <imm35@pitt.edu>
2025-01-24 14:52:04 -0500

    Created program2.py

---
+++ program2.py
@@ -0,0 +1,4 @@
+#Author: Isaac Martinec
+itemPurchased = input('Please enter how many items you purchased: ')
+itemCost = input('Please enter the price of the item: ')
+print('Your total is: ' + str(int(itemPurchased)*float(itemCost)))
